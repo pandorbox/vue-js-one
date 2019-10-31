f42675155aafc1b188a26d5bfbfc42c9c7ba8d0a
qijiankan <admin@qq.com>
2019-10-31 09:05:31 +0800

    0.0.1

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -1,32 +1,30 @@
 //更改用户状态信息
-export const userStatus = (state,user) => {
-  if (user){
+export const userStatus = (state, user) => {
+  if (user) {
     state.currentUser = user;
-    state.isLogin = true
-  } else if (user == null){
+    state.isLogin = true;
+  } else if (user == null) {
     //登出的时候，清空sessionStorage里的东西
-    sessionStorage.setItem("userName",null);
-    sessionStorage.setItem("userToken","");
-    sessionStorage.setItem("userImage",null);
+    sessionStorage.setItem("userName", null);
+    sessionStorage.setItem("userToken", "");
+    sessionStorage.setItem("userImage", null);
     state.currentUser = null;
     state.isLogin = false;
     state.token = "";
     state.userImage = null;
   }
-}
-export const userImageGet = (state,imageUrl) => {
-  if (imageUrl){
+};
+export const userImageGet = (state, imageUrl) => {
+  if (imageUrl) {
     state.userImage = imageUrl;
-  } else if (imageUrl == null){
+  } else if (imageUrl == null) {
     state.userImage = null;
   }
-}
-
-
-export const userToken = (state,userToken) => {
+};
+export const userToken = (state, userToken) => {
   if (userToken) {
     state.token = userToken;
   } else {
     state.token = null;
   }
-}
+};
